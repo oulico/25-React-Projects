f00b9c4e42e19e57c786ec3be59d6a6feda878c4
JH <manemis@gmail.com>
2022-10-23 12:58:05 +0900

    todo list

--- 10-grocery-bud/setup/src/List.js
+++ 10-grocery-bud/setup/src/List.js
@@ -1,7 +1,39 @@
-import React from 'react'
-import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
-}
+import React from "react";
+import { FaEdit, FaTrash } from "react-icons/fa";
+const List = ({ items, removeItem, editItem }) => {
+  return (
+    <div className="grocery-list">
+      {items.map((item) => {
+        console.log(item);
+        const { id, title } = item;
+        return (
+          <article key={id} className="grocery-item">
+            <p className="title">{title} </p>
+            <div className="btn-container">
+              <button
+                type="button"
+                className="edit-btn"
+                onClick={() => {
+                  editItem(id);
+                  console.log(id);
+                }}
+              >
+                <FaEdit />
+              </button>
+              <button
+                type="button"
+                className="delete-btn"
+                onClick={() => removeItem(id)}
+              >
+                <FaTrash />
+              </button>
+              <button></button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
+};
 
-export default List
+export default List;
